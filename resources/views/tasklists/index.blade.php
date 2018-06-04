ba082626029d644ec0f08154b1cd465249bf30e3
koki.ogawa <koki.ogawa@rakuten.com>
2018-06-04 01:49:54 +0000

    CRUD messages

---
+++ resources/views/tasklists/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('content')
+ <h1>メッセージ一覧</h1>
+ @if (count($tasklists) > 0)
+ <ul>
+ @foreach ($tasklists as $tasklist)
+ <li>{{ $tasklist->content }}</li>
+ @endforeach
+ </ul>
+ @endif
+ 
+ {!! link_to_route('messages.create', '新規メッセージの投稿') !!}
+ 
+@endsection
