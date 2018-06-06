9878af1c7491195bb2ac176592925d511ce5bec7
koki.ogawa <koki.ogawa@rakuten.com>
2018-06-06 07:24:42 +0000

    bootstrap design

--- resources/views/tasklists/show.blade.php
+++ resources/views/tasklists/show.blade.php
@@ -2,12 +2,24 @@
 @section('content')
  <h1>id = {{ $tasklist->id }} のタスク詳細ページ</h1>
  
- <p>ステータス: {{ $tasklist->status }}</p>
- <p>タスク内容：{{ $tasklist->content }}</p>
-  {!! link_to_route('tasklists.edit', 'このタスクを編集', ['id' => $tasklist->id]) !!}
-   {!! Form::model($tasklist, ['route' => ['tasklists.destroy', $tasklist->id], 'method' => 'delete']) !!}
- {!! Form::submit('削除') !!}
- {!! Form::close() !!}
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $tasklist->id }}</td>
+        </tr>
+        <tr>
+            <th>ステータス</th>
+            <td>{{ $tasklist->status }}</td>
+        </tr>
+        <tr>
+            <th>タスク内容</th>
+            <td>{{ $tasklist->content }}</td>
+        </tr>
+    </table>
+    {!! link_to_route('tasklists.edit', 'このタスクを編集', ['id' => $tasklist->id], ['class' => 'btn btn-default']) !!}
+    {!! Form::model($tasklist, ['route' => ['tasklists.destroy', $tasklist->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
 
 
 @endsection
